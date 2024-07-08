29eea0f72e9cf156566f1a1f76763e9290b81b04
joalopezmo <joalopezmo@uoc.edu>
2024-07-09 00:38:41 +0200

    chore: Update dependencies and configuration files
    This commit includes updates to the project's dependencies and configuration files. It also initializes the environment variables and the migration to the db with prismaORM.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('ProductsMS-Main');
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
@@ -14,6 +15,6 @@ async function bootstrap() {
   );
 
   await app.listen(envs.port);
-  console.log(`App is running on ${envs.port}`);
+  logger.log(`App is running on ${envs.port}`);
 }
 bootstrap();
