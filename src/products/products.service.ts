c900b935fe7578804d0337609509a2ea0bf7d441
joalopezmo <joalopezmo@uoc.edu>
2024-08-02 13:37:53 +0200

    chore: Update ProductsService to handle NotFoundException
    This commit updates the `ProductsService` to handle the `NotFoundException` when retrieving a product by ID. If the product is not found, the service will throw the exception, providing a clear error message.

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -35,8 +40,15 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where: { id },
+    });
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    } else {
+      return product;
+    }
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
