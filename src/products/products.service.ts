21abed3b41353a368b1ff4a71020235fe0c4d882
joalopezmo <joalopezmo@uoc.edu>
2024-08-02 10:15:21 +0200

    chore: Update CreateProductDto to use number type for price
    This commit updates the `CreateProductDto` class to use the `number` type for the `price` property instead of `string`. This change ensures that the `price` value is treated as a number throughout the application.

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -13,11 +13,13 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    return this.product.create({
+      data: createProductDto,
+    });
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.product.findMany({});
   }
 
   findOne(id: number) {
